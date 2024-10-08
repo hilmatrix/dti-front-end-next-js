d2cdaee5391633839883e5e232d86dcf025a684d
Hilman Mauludin <32255027+hilmatrix@users.noreply.github.com>
2024-10-08 16:04:13 +0700

    added formix for form submission

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import React from 'react';
 import ContactInfo from '../../components/ContactInfo/index';
 import Footer from '../../components/Footer/index';
--- src/components/SubmitMessage/index.tsx
+++ src/components/SubmitMessage/index.tsx
@@ -1,20 +1,45 @@
+
+import { useFormik } from 'formik';
 import React from 'react';
+import * as Yup from 'yup';
 import "./style.css";
 
 const Index: React.FC = () => {
+  const formik = useFormik({
+    initialValues : {
+      name : "",
+      email : "",
+      subject : "",
+      message : ""
+    },
+    validationSchema : Yup.object({
+      name : Yup.string().required('Name is required'),
+      email : Yup.string().email('Invalid email address').required('Email is required'),
+      subject : Yup.string().required('Subject is required'),
+      message : Yup.string().required('Message is required')
+    }),
+    onSubmit : (values) => {
+      console.log(values);
+    }
+  });
+
   return (
-    <div className='submit-message'>
+    <form onSubmit={formik.handleSubmit} className='submit-message'>
         <h1>Lets build something cool together</h1>
         <label>Name</label>
-        <input type='text' placeholder='Hilman Mauludin'></input>
+        <input id="name" type='text' {...formik.getFieldProps('name')} placeholder='Hilman Mauludin'></input>
+        {(formik.touched.name && formik.errors.name) ? (<div className='text-[#FF0000]'>{formik.errors.name}</div>) : null}
         <label>Email</label>
-        <input type='text' placeholder='hilmaul@gmail.com'></input>
+        <input id="email" type='text' {...formik.getFieldProps('email')} placeholder='hilmaul@gmail.com'></input>
+        {(formik.touched.email && formik.errors.email) ? (<div className='text-[#FF0000]'>{formik.errors.email}</div>) : null}
         <label>Subject</label>
-        <input type='text' placeholder='For web design work Enquire'></input>
+        <input id="subject" type='text' {...formik.getFieldProps('subject')} placeholder='For web design work Enquire'></input>
+        {(formik.touched.subject && formik.errors.subject) ? (<div className='text-[#FF0000]'>{formik.errors.subject}</div>) : null}
         <label>Message</label>
-        <textarea className="textarea" placeholder='Type your message'></textarea>
-        <button>Submit</button>
-    </div>
+        <textarea id="message" {...formik.getFieldProps('message')} className="textarea" placeholder='Type your message'></textarea>
+        {(formik.touched.message && formik.errors.message) ? (<div className='text-[#FF0000]'>{formik.errors.message}</div>) : null}
+        <button type="submit" disabled={formik.isSubmitting}> {formik.isSubmitting ? "Submitting..." : "Submit"}</button>
+    </form>
   );
 }
 
